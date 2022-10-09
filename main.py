dc20a49332ed551ef102f9dc0c9d7644b07c6192
jan <jan.kuczaj@gmail.com>
2022-10-09 16:54:59 +0200

    added detailed comments

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ def get_config():
     Get default configs from config/config
     If a setting is missing in the config set it to a default value
     """
-    prefix: bool = False
-    status: bool = False
+    prefix: bool = False  # used to check if prefix was set multiple times
+    status: bool = False  # used to check if status was set multiple times
 
-    line_num: int = 0
+    line_num: int = 0  # line count
 
+    # used to store configs
     bot_prefix: str = ""
     bot_status: str = ""
 
@@ -22,15 +23,15 @@ def get_config():
         line_num += 1
         if line.startswith(";"):  # ignore comments
             continue
-        elif line.startswith("prefix:"):
-            if not prefix:
+        elif line.startswith("prefix:"):  # if the line in config file starts with "prefix:"
+            if not prefix:  # and the prefix wasn't set before
                 prefix = True
-                bot_prefix = line.replace("prefix:", "")
-                bot_prefix.replace(" ", "")
+                bot_prefix = line.replace("prefix:", "")  # remove "prefix:"
+                bot_prefix.replace(" ", "")  # and whitespaces
                 print("Config: prefix set to " + bot_prefix)
             else:
                 print("Config-error: [line " + str(line_num) + "] prefix was set before.")
-        elif line.startswith("status:"):
+        elif line.startswith("status:"):  # do the same for "status:"
             if not status:
                 status = True
                 bot_status = line.replace("status:", "")
@@ -38,8 +39,9 @@ def get_config():
                 print("Config: status set to " + bot_status)
             else:
                 print("Config-error: [line " + str(line_num) + "] status was set before.")
-        else:
+        else:  # return error when user is trying to change non-existent setting
             print("Config-error: [line " + str(line_num) + "] unrecognized value: " + line)
+    # if there are values missing in config file, set them to defaults
     if not prefix:
         bot_prefix = ","
         print("Config: prefix set to a default value    " + bot_prefix)
@@ -73,4 +75,5 @@ if token.isspace():  # exit if token is empty
     print("No token specified! Put token in token/.token")
     exit()
 
+# run the bot
 client.run(token)
